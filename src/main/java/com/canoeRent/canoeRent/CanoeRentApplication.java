0778d7bb396176b38b66d2a0400aa69582be37da
MaciejSwiderski <maciejswiderski@wp.pl>
2018-06-09 15:32:07 +0200

    first

--- src/main/java/com/canoeRent/canoeRent/CanoeRentApplication.java
+++ src/main/java/com/canoeRent/canoeRent/CanoeRentApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CanoeRentApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CanoeRentApplication.class, args);
+		System.out.println("hello world");
 	}
 }
