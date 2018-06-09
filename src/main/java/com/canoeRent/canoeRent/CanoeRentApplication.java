2ab65654e6466825c50019c8bfd67a48fb8d76d2
Filip <filipmel@gmail.com>
2018-06-09 14:55:49 +0200

    Spring initializer

---
+++ src/main/java/com/canoeRent/canoeRent/CanoeRentApplication.java
@@ -0,0 +1,12 @@
+package com.canoeRent.canoeRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanoeRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanoeRentApplication.class, args);
+	}
+}
