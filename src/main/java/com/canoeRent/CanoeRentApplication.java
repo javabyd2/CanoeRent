786dbae2c69ab5c1ae379382b01eb2e76477796b
Filip <filipmel@gmail.com>
2018-06-10 10:01:12 +0200

    Packeges added

--- src/main/java/com/canoeRent/CanoeRentApplication.java
+++ src/main/java/com/canoeRent/CanoeRentApplication.java
@@ -1,4 +1,4 @@
-package com.canoeRent.canoeRent;
+package com.canoeRent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/canoeRent/CanoeRentApplicationTests.java
+++ src/test/java/com/canoeRent/CanoeRentApplicationTests.java
@@ -1,4 +1,4 @@
-package com.canoeRent.canoeRent;
+package com.canoeRent;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
